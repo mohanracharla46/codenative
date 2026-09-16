05f722a56358197ef225ab41f4cebabe8069b897
mohanracharla46 <mohanracharla46@gmail.com>
2026-09-16 12:27:40 +0530

    Update root service worker sw.js with 5gvci.com and update CSP

--- static/sw.js
+++ static/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11813694
+    "domain": "5gvci.com",
+    "zoneId": 11813860
 };
 self.lary = "";
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw');
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
